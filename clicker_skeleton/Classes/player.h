187bbd43563254a2a3262c311890af2611210859
haoRchen <hao.ran.chen24@gmail.com>
2016-07-26 18:54:18 -0400

    Working on sign-in page. Toying around with textbox and console feedback. Successful! Need to add login button ui, facebook and google login API and organize layout

---
+++ clicker_skeleton/Classes/player.h
@@ -0,0 +1,20 @@
+//
+//  player.h
+//  MyCppGame
+//
+//  Created by Hao ran chen on 2016-07-26.
+//
+//
+
+#ifndef player_h
+#define player_h
+
+class player{
+    string username;//displayed ingame
+    string email_address;//signing up only
+    string email_password;//signing up only
+    
+};
+
+
+#endif /* player_h */
