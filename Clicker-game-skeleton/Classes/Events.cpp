168704ec831248d11965b96a5cfb77f5c2187f9c
haoRchen <hao.ran.chen24@gmail.com>
2016-08-25 05:49:56 -0400

    event setup via comments

---
+++ Clicker-game-skeleton/Classes/Events.cpp
@@ -0,0 +1,9 @@
+//
+//  Events.cpp
+//  MyGame
+//
+//  Created by Hao ran chen on 2016-08-25.
+//
+//
+
+#include "Events.h"
---
+++ Clicker-game-skeleton/Classes/Events.h
@@ -0,0 +1,32 @@
+//
+//  Events.hpp
+//  MyGame
+//
+//  Created by Hao ran chen on 2016-08-25.
+//
+//
+
+#ifndef Events_h__
+#define Events_h__
+
+#include <stdio.h>
+#include "cocos2d.h"
+
+USING_NS_CC;
+class Events : public Layer
+{
+private:
+    //needed: randomize event, create randomizer to pick an event and activate via a switch
+    //label to display message
+    //a number to reference event
+    //sprite for popup
+    //button to ackowledge event occurence
+    //database pointer
+public:
+    //getters and setters for private variables
+    //switch function to push the proper message to the label
+    
+};
+
+
+#endif /* Events_h__ */
